ec36565bd0f866fa26033f65c1bae5225e44a68a
AsmaqAnis <141695339+AsmaqAnis@users.noreply.github.com>
2023-08-15 10:12:55 +0800

    Contact.js

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -9,9 +9,45 @@ const Contact = () => {
       <NavBar />
       <HeroImg2 heading="CONTACT." text="Lets have a chat" />
       <Form />
+      <TextFile>
+        Hello
+      </TextFile>
+      <TextFile2 heading="CONTACT." text="Lets have a chat"  
+      click={click} setClick={setClick} />
       <Footer />
     </div>
   )
 }
 
 export default Contact
+
+//global component that cannot be export
+//function with '()' does not have return
+const TextFile = ({children}) => (
+  <div style={{backgroundColor:'blue'}}>
+    {children}
+  </div>
+)
+
+//function with '{}' have return
+const TextFile2 = (props) => {
+  console.log(props)
+  //local component
+  const TextFile3 = (props2) => {
+    console.log(props2)
+    return(
+      <p>{props2.heading}</p>
+    )
+  }
+  return(
+  <div style={{backgroundColor:'green'}}>
+    {/* property inheritance from TextFile2 to TextFile3 
+        {...props} <- to inherit property with same name*/}
+    <TextFile3 heading3={props.heading}  {...props}/>
+    {/* inherit useEffect in both component TextFile2 & TextFile3 */}
+    <button onClick={() => props.setClick(props.click+1)}>
+      Click Me
+    </button>
+  </div>
+  )
+}
